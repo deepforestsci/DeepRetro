b8e2a02c08235d8620d15cc78bd5a8691e8a3229
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2024-12-19 23:29:17 +0530

    update metadata agents

--- src/prithvi.py
+++ src/prithvi.py
@@ -8,6 +8,7 @@ from src.utils.parse import format_output
 from src.rec_prithvi import rec_run_prithvi
 from src.utils.job_context import logger as context_logger
 from src.utils.custom_logging import add_job_specific_handler
+from src.metadata import reagent_agent, conditions_agent, literature_agent
 
 root_dir = rootutils.setup_root(__file__,
                                 indicator=".project-root",
@@ -32,9 +33,39 @@ def run_prithvi(molecule):
     try:
         result_dict, solved = rec_run_prithvi(molecule, job_id)
         output_data = format_output(result_dict)
+        output_data = add_metadata(output_data)
         return output_data
     finally:
         # Clean up handlers
         log._logger.removeHandler(handler)
         handler.close()
         context_logger.reset(token)
+
+
+def add_metadata(output_data: dict) -> dict:
+    """method to add metadata to reaction metrics
+
+    Parameters
+    ----------
+    output_data : dict
+        json output without metadata
+
+    Returns
+    -------
+    dict
+        json output with metadata
+    """
+    for idx, step in enumerate(output_data['steps']):
+        reagents = reagent_agent(step['reactants'], step['product'])
+        output_data['steps'][idx]['reagents'].append(reagents)
+
+        conditions = conditions_agent(step['reactants'], step['product'],
+                                      step['reagents'])
+        output_data['steps'][idx]['conditions'] = conditions
+
+        literature = literature_agent(step['reactants'], step['product'],
+                                      step['reagents'], conditions)
+        output_data['steps'][idx]['reactionmetrics'][0][
+            'closestliterature'] = literature
+
+    return output_data
