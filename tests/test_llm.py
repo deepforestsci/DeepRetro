281e106c8ee97cee698d21e17c80bdf7997912e6
Rahul Jana <i.rahuljana@gmail.com>
2025-02-18 13:45:29 +0530

    Added functional tests.

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -1,24 +1,33 @@
 import os
 import ast
+import pytest
 
 import rootutils
 root_dir = rootutils.setup_root(".", indicator=".project-root", pythonpath=True)
 
-import pytest
-from src.utils.llm import *
-from src.variables import *
+from src.utils.llm import call_LLM, split_cot_json, split_json_openAI, split_json_deepseek
+from src.variables import OPENAI_MODELS
 
-from tests.variables_test import *
+    
 
-def test_call_llm_SUCCESS():
+def test_call_llm_success():
+
+    from tests.variables_test import VALID_SMILE_STRING
+
     status_code, res_text = call_LLM(molecule=VALID_SMILE_STRING)
     assert status_code == 200
 
-def test_call_llm_openai_SUCCESS():
+def test_call_llm_openai_success():
+
+    from tests.variables_test import VALID_SMILE_STRING
+
     status_code, res_text = call_LLM(molecule=VALID_SMILE_STRING, LLM="gpt-4o")
     assert status_code == 200
 
-def test_all_openai_SUCCESS():
+def test_all_openai_success():
+
+    from tests.variables_test import VALID_SMILE_STRING
+
     successful_tests = []
     failed_tests = []
     print("models: ", OPENAI_MODELS)
@@ -33,31 +42,44 @@ def test_all_openai_SUCCESS():
     if failed_tests:
         print(f"Failed models: {failed_tests}")
 
-def test_call_llm_deepseek_SUCCESS():
+def test_call_llm_deepseek_success():
+
+    from tests.variables_test import VALID_SMILE_STRING
+
     status_code, res_text = call_LLM(molecule=VALID_SMILE_STRING, LLM="gpt-4o")
     assert status_code == 200
 
-def test_split_cot_json_SUCCESS():
+def test_split_cot_json_success():
+    
+    from tests.variables_test import VALID_CLAUDE_RESPONSE
+
     status_code, thinking_steps, json_content = split_cot_json(VALID_CLAUDE_RESPONSE)
     assert status_code == 200
 
-def test_split_cot_json_FAIL_501():
+def test_split_cot_json_fail_501():
+    
+    from tests.variables_test import EMPTY_RESPONSE
+    
     status_code, thinking_steps, json_content = split_cot_json(EMPTY_RESPONSE)
     assert status_code == 501
 
-def test_split_json_openai_SUCCESS():
-    status_code, res_OpenAI = call_LLM(molecule="CC(=O)CC", LLM="gpt-4o")
+def test_split_json_openai_success():
+    
+    from tests.variables_test import VALID_SMILE_STRING
+
+    status_code, res_OpenAI = call_LLM(molecule=VALID_SMILE_STRING, LLM="gpt-4o")
+    
     assert status_code == 200
 
-def test_split_json_openai_FAIL_502():
+def test_split_json_openai_fail_502():
     status_code, res_OpenAI = split_json_openAI("")
     assert status_code == 502
 
-def test_split_json_deepseek_SUCCESS():
+def test_split_json_deepseek_success():
     status_code, thinking_steps, json_content = split_json_deepseek(VALID_CLAUDE_RESPONSE)
     assert status_code == 200
 
-def test_split_json_deepseek_FAIL_503():
+def test_split_json_deepseek_fail_503():
     status_code, thinking_steps, json_content = split_json_deepseek(EMPTY_RESPONSE)
     assert status_code == 503
 
