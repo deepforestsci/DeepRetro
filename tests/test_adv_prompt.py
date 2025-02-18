01760d64d0c9a0b723382c91dda05479a7b09914
Rahul Jana <i.rahuljana@gmail.com>
2025-02-19 03:23:16 +0530

    Added advance prompt tests.

--- tests/test_adv_prompt.py
+++ tests/test_adv_prompt.py
@@ -11,11 +11,33 @@ from src.utils.llm import call_LLM, split_cot_json, split_json_openAI, split_jso
 from src.variables import OPENAI_MODELS
 from tests.variables_test import VALID_SMILE_STRING
 
+DEEPSEEK_MODEL_ADV = "deepinfra/deepseek-ai/DeepSeek-R1:adv"
+OPENAI_MODEL_ADV = "claude-3-opus-20240229:adv"
+CLAUDE_MODEL_ADV = "claude-3-opus-20240229:adv"
+
+print("Used smile string: ", VALID_SMILE_STRING)
 
 def test_claude_adv_success():
 
     status_code, res_text = call_LLM(molecule=VALID_SMILE_STRING,
-                                     LLM="claude-3-opus-20240229:adv")
+                                     LLM=CLAUDE_MODEL_ADV)
+    if not res_text:
+        assert status_code == 404
+    assert status_code == 200
+
+def test_openai_adv_success():
+
+    status_code, res_text = call_LLM(molecule=VALID_SMILE_STRING,
+                                     LLM=OPENAI_MODEL_ADV)
+    if not res_text:
+        assert status_code == 404
+    assert status_code == 200
+
+
+def test_deepseek_adv_success():
+
+    status_code, res_text = call_LLM(molecule=VALID_SMILE_STRING,
+                                     LLM=DEEPSEEK_MODEL_ADV)
     if not res_text:
         assert status_code == 404
     assert status_code == 200
