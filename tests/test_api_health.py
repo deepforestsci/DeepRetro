d710333340dd5431a57cddf77f7e5bd023d4f579
Rahul Jana <i.rahuljana@gmail.com>
2025-02-21 21:27:50 +0530

    Added api health tests.

---
+++ tests/test_api_health.py
@@ -0,0 +1,248 @@
+import requests
+import json
+import pytest
+
+import rootutils
+root_dir = rootutils.setup_root(".",
+                                indicator=".project-root",
+                                pythonpath=True)
+
+from tests.variables_test import MOLECULE_1, BASE_URL, ENDPOINTS, X_API_KEY, OPENAI_MODEL, DEEPSEEK_MODEL, CLAUDE_MODEL
+
+# Health
+# OpenAI Health
+def test_health_openai_m1p1_success():
+  
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+      "smiles": MOLECULE_1,
+      "advanced_model": "True",
+      "advanced_prompt": "True",
+      "model_version": OPENAI_MODEL})
+
+    headers = {
+      'x-api-key': X_API_KEY,
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_openai_m0p1_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "True",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_openai_m1p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "True",
+        "advanced_prompt": "False",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_openai_m0p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "False",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+# Deepseek Health
+def test_health_deepseek_m1p1_success():
+  
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+      "smiles": MOLECULE_1,
+      "advanced_model": "True",
+      "advanced_prompt": "True",
+      "model_version": DEEPSEEK_MODEL})
+
+    headers = {
+      'x-api-key': X_API_KEY,
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_deepseek_m0p1_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "True",
+        "model_version": DEEPSEEK_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_deepseek_m1p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "True",
+        "advanced_prompt": "False",
+        "model_version": DEEPSEEK_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_deepseek_m0p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "False",
+        "model_version": DEEPSEEK_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+# Claude Health
+def test_health_claude_m1p1_success():
+  
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+      "smiles": MOLECULE_1,
+      "advanced_model": "True",
+      "advanced_prompt": "True",
+      "model_version": CLAUDE_MODEL})
+
+    headers = {
+      'x-api-key': X_API_KEY,
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_claude_m0p1_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "True",
+        "model_version": CLAUDE_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_claude_m1p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "True",
+        "advanced_prompt": "False",
+        "model_version": CLAUDE_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_claude_m0p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['health']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "False",
+        "model_version": CLAUDE_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+if __name__ == '__main__':
+  pytest.main()
