f69bf8246e747b382b156acb482d3069f13446e6
Rahul Jana <i.rahuljana@gmail.com>
2025-02-22 21:18:08 +0530

    Added test reruns.

--- tests/test_rerun_retrosynthesis.py
+++ tests/test_rerun_retrosynthesis.py
@@ -7,7 +7,83 @@ root_dir = rootutils.setup_root(".",
                                 indicator=".project-root",
                                 pythonpath=True)
 
-from tests.variables_test import MOLECULE_1, BASE_URL, ENDPOINTS, X_API_KEY, DEEPSEEK_MODEL, CLAUDE_MODEL, EMPTY_RESPONSE, PISTACHIO_MODEL
+from tests.variables_test import MOLECULE_1, BASE_URL, ENDPOINTS, X_API_KEY, OPENAI_MODEL, DEEPSEEK_MODEL, CLAUDE_MODEL, EMPTY_RESPONSE, PISTACHIO_MODEL
+
+
+def test_health_openai_m1p1_success():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "True",
+        "advanced_prompt": "True",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_openai_m1p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "True",
+        "advanced_prompt": "False",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_openai_m0p1_success():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "True",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+
+
+def test_health_openai_m0p0_success():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "advanced_model": "False",
+        "advanced_prompt": "False",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
 
 
 def test_health_deepseek_m1p1_success():
@@ -163,6 +239,82 @@ def test_health_claude_m0p0_success():
 
 
 # -------------------------------------------------------- #
+
+
+def test_health_openai_m1p1_fail():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": EMPTY_RESPONSE,
+        "advanced_model": "True",
+        "advanced_prompt": "True",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 500
+
+
+def test_health_openai_m1p0_fail():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": EMPTY_RESPONSE,
+        "advanced_model": "True",
+        "advanced_prompt": "False",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 500
+
+
+def test_health_openai_m0p1_fail():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": EMPTY_RESPONSE,
+        "advanced_model": "False",
+        "advanced_prompt": "True",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 500
+
+
+def test_health_openai_m0p0_fail():
+    url = f"{BASE_URL}{ENDPOINTS['rerun_retro']}"
+
+    payload = json.dumps({
+        "smiles": EMPTY_RESPONSE,
+        "advanced_model": "False",
+        "advanced_prompt": "False",
+        "model_version": OPENAI_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 500
 
 
 def test_health_deepseek_m1p1_fail():
