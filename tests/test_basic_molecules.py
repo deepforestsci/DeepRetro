a290f6c15cc9aa7cdc99ad3e752bf80adb53d432
Rahul Jana <i.rahuljana@gmail.com>
2025-02-23 20:25:24 +0530

    Added basic molecules tests.

--- tests/test_basic_molecules.py
+++ tests/test_basic_molecules.py
@@ -1,3 +1,7 @@
+'''
+Note: This set of tests takes quite some time to run, as it runs over 86 BASIC_MOLECULES listed in the src/variables.py
+'''
+
 import os
 import ast
 import pytest
@@ -5,14 +9,7 @@ import pytest
 import rootutils
 root_dir = rootutils.setup_root(".", indicator=".project-root", pythonpath=True)
 
-from src.utils.llm import call_LLM, split_cot_json, split_json_openAI, split_json_deepseek
-from src.variables import OPENAI_MODELS, DEEPSEEK_MODELS
-
-claude_adv = "claude-3-opus-20240229:adv"
-
-openai_adv = "gpt-4o:adv"
-
-deepseek_adv = "azure_ai/DeepSeek-R1:adv"
+from src.utils.llm import call_LLM
 
 def test_basic_molecules():
     from src.variables import BASIC_MOLECULES
@@ -37,8 +34,32 @@ def test_basic_molecules():
     if failed_molecules:
         print(f"Failed molecules: {failed_molecules}")
 
-def test_basic_molecules_adv_prompt_claude(llm=claude_adv):
+# def test_basic_molecules_adv_prompt_openai(): 
+#     from src.variables import BASIC_MOLECULES
+#     from tests.variables_test import OPENAI_ADV_MODEL
+
+#     print("BASIC_MOLECULES: ", BASIC_MOLECULES)
+#     successful_molecules = []
+#     failed_molecules = []
+
+#     for molecule in BASIC_MOLECULES:
+#         try:
+#             status_code, res = call_LLM(molecule=molecule, LLM = OPENAI_ADV_MODEL)
+#             assert status_code == 200
+            
+#             if not res:
+#                 raise Exception("Response is empty.")
+            
+#             successful_molecules.append(molecule)
+#         except Exception as e:
+#             print(f"Error: {e}\n Molecule: {molecule}")
+#             failed_molecules.append(molecule)
+#     if failed_molecules:
+#         print(f"Failed molecules: {failed_molecules}")
+
+def test_basic_molecules_adv_prompt_deepseek():
     from src.variables import BASIC_MOLECULES
+    from tests.variables_test import DEEPSEEK_FIREWORKS_MODEL
 
     print("BASIC_MOLECULES: ", BASIC_MOLECULES)
     successful_molecules = []
@@ -46,51 +67,8 @@ def test_basic_molecules_adv_prompt_claude(llm=claude_adv):
 
     for molecule in BASIC_MOLECULES:
         try:
-            status_code, res = call_LLM(molecule=molecule, LLM = llm)
-            assert status_code == 200
-            
-            if not res:
-                raise Exception("Response is empty.")
-            
-            successful_molecules.append(molecule)
-        except Exception as e:
-            print(f"Error: {e}\n Molecule: {molecule}")
-            failed_molecules.append(molecule)
-    if failed_molecules:
-        print(f"Failed molecules: {failed_molecules}")
-
-def test_basic_molecules_adv_prompt_openai(llm=openai_adv): 
-    from src.variables import BASIC_MOLECULES
-
-    print("BASIC_MOLECULES: ", BASIC_MOLECULES)
-    successful_molecules = []
-    failed_molecules = []
-
-    for molecule in BASIC_MOLECULES:
-        try:
-            status_code, res = call_LLM(molecule=molecule, LLM = llm)
-            assert status_code == 200
-            
-            if not res:
-                raise Exception("Response is empty.")
-            
-            successful_molecules.append(molecule)
-        except Exception as e:
-            print(f"Error: {e}\n Molecule: {molecule}")
-            failed_molecules.append(molecule)
-    if failed_molecules:
-        print(f"Failed molecules: {failed_molecules}")
-
-def test_basic_molecules_adv_prompt_deepseek(llm=deepseek_adv):
-    from src.variables import BASIC_MOLECULES
-
-    print("BASIC_MOLECULES: ", BASIC_MOLECULES)
-    successful_molecules = []
-    failed_molecules = []
-
-    for molecule in BASIC_MOLECULES:
-        try:
-            status_code, _ = call_LLM(molecule=molecule, LLM = llm)
+            status_code, _ = call_LLM(molecule=molecule,
+                                      LLM = DEEPSEEK_FIREWORKS_MODEL)
             assert status_code == 200
             successful_molecules.append(molecule)
         except Exception as e:
