292db7cf5334f576e67e280a7249035a9e478cfb
Rahul Jana <i.rahuljana@gmail.com>
2025-05-16 21:41:37 +0530

    Added stability and hallucination checks for deepseek model.

--- tests/api-tests/test_api_deepseek.py
+++ tests/api-tests/test_api_deepseek.py
@@ -289,5 +289,73 @@ def test_retrosynthesis_deepseek_uspto_p0_success():
     assert ['dependencies', 'steps'] == list(response.json().keys())
 
 
+def test_stability_hallucination_claude_p0_success():
+    """Test retrosynthesis endpoint with
+    Model: DeepSeek
+    Model Version: Pistachio
+    Prompt: Advance.
+
+    Asserts
+    -------
+    status_code : int
+        200 for a successful request.
+    response : dict
+        The response should be a dictionary with keys ['dependencies', 'steps'].
+    """
+    url = f"{BASE_URL}{ENDPOINTS['retrosynthesis']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "stability_flag": "True",
+        "hallucination_check": "True",
+        "advanced_prompt": "False",
+        "llm": DEEPSEEK_FIREWORKS_MODEL,
+        "model_version": USPTO_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert ['dependencies', 'steps'] == list(response.json().keys())
+
+def test_stability_hallucination_claude_p1_success():
+    """Test retrosynthesis endpoint with
+    Model: DeepSeek
+    Model Version: Pistachio
+    Prompt: Advance.
+
+    Asserts
+    -------
+    status_code : int
+        200 for a successful request.
+    response : dict
+        The response should be a dictionary with keys ['dependencies', 'steps'].
+    """
+    url = f"{BASE_URL}{ENDPOINTS['retrosynthesis']}"
+
+    payload = json.dumps({
+        "smiles": MOLECULE_1,
+        "stability_flag": "True",
+        "hallucination_check": "True",
+        "advanced_prompt": "True",
+        "llm": DEEPSEEK_FIREWORKS_MODEL,
+        "model_version": PISTACHIO_MODEL})
+
+    headers = {
+        'x-api-key': X_API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert ['dependencies', 'steps'] == list(response.json().keys())
+
 if __name__ == '__main__':
     pytest.main()
