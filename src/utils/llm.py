1761fd6df8b0ffc768ead3fd5230354101575f5e
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2026-02-24 00:34:08 +0530

    remove logs

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -17,7 +17,6 @@ from src.utils.job_context import logger as context_logger
 from src.utils.stability_checks import stability_checker
 from src.utils.hallucination_checks import hallucination_checker
 from src.protecting_group import mask_protecting_groups_multisymbol
-from src.deprotecting_group import unmask_protecting_groups_multisymbol, get_protecting_group_info
 
 load_dotenv()
 
@@ -26,6 +25,7 @@ litellm.success_callback = ["langfuse"]
 litellm.drop_params = True
 
 from src.utils.langfuse_config import get_langfuse_metadata
+
 ENABLE_LOGGING = False if os.getenv("ENABLE_LOGGING",
                                     "true").lower() == "false" else True
 
