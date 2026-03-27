1485eeed74df30aff4dbb4d52251810344bf6ced
Rishikesh Panda <rishipanda2003@gmail.com>
2026-03-27 22:17:54 +0530

    draft autosolve

--- deepretro/utils/__init__.py
+++ deepretro/utils/__init__.py
@@ -1,8 +1,11 @@
 from .domain_features import extract_domain_features_single, NUM_DOMAIN_FEATURES
 from .metrics import find_optimal_threshold
+from .autosolve import autosolve, autosolve_async
 
 __all__ = [
     "extract_domain_features_single",
     "NUM_DOMAIN_FEATURES",
     "find_optimal_threshold",
+    "autosolve",
+    "autosolve_async",
 ]
